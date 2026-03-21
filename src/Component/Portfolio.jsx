ca4e74d3839869f9996d96d8298b4c14fb3598ee
suvish <suvishjain@gmail.com>
2026-03-21 21:44:37 +0530

    Fixed Deployment Issues portfolio

--- src/Component/Portfolio.jsx
+++ src/Component/Portfolio.jsx
@@ -55,7 +55,7 @@ function useInView(threshold = 0.15) {
     const obs = new IntersectionObserver(([e]) => { if (e.isIntersecting) setVisible(true); }, { threshold });
     if (ref.current) obs.observe(ref.current);
     return () => obs.disconnect();
-  }, []);
+  }, [threshold]);
   return [ref, visible];
 }
 
